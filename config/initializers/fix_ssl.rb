7013de2c9d4950881b552d1084fa3d2c50ba5601
Reginald Pawle <rbpawle@gmail.com>
2012-04-20 15:41:28 -0400

    changed config.assets.compile to true

--- config/initializers/fix_ssl.rb
+++ config/initializers/fix_ssl.rb
@@ -1,3 +1,9 @@
+# config/initializers/fix_ssl.rb
+# 
+# Work around errors that look like:
+#
+#   SSL_connect returned=1 errno=0 state=SSLv3 read server certificate B: certificate verify failed (OpenSSL::SSL::SSLError)
+
 require 'open-uri'
 require 'net/https'
 
@@ -6,7 +12,10 @@ module Net
     alias_method :original_use_ssl=, :use_ssl=
     
     def use_ssl=(flag)
-      self.ca_file = Rails.root.join('lib/assets/ca-bundle.crt').to_s
+      # Ubuntu
+      if File.exists?('/etc/ssl/certs')
+        self.ca_path = '/etc/ssl/certs'
+      
       self.verify_mode = OpenSSL::SSL::VERIFY_PEER
       self.original_use_ssl = flag
     end
